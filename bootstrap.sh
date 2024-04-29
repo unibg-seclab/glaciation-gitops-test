c5f2b325d70f639fd1bc66b8abe8b8c9c05f46c8
Matthew Rossi <mattw.rossi@gmail.com>
2024-04-29 16:42:52 +0200

    Automate installation and configuration of ArgoCD

---
+++ bootstrap.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# By setting the KUBECONFIG environment variable it is possible to run the
+# setup against different kubernetes environments
+
+echo "[*] Install ArgoCD"
+helm repo add argo https://argoproj.github.io/argo-helm
+helm install \
+    --create-namespace \
+    --namespace argocd \
+    --set configs.repositories[0].type=git \
+    --set configs.repositories[0].url=https://github.com/unibg-seclab/glaciation-gitops-test.git \
+    --set server.service.type=NodePort \
+    my-release argo/argo-cd
+
+echo -e '\nWaiting for the rollout of the service...'
+kubectl -n argocd rollout status deploy/my-release-argocd-applicationset-controller
+kubectl -n argocd rollout status deploy/my-release-argocd-dex-server
+kubectl -n argocd rollout status deploy/my-release-argocd-notifications-controller
+kubectl -n argocd rollout status deploy/my-release-argocd-redis
+kubectl -n argocd rollout status deploy/my-release-argocd-repo-server
+kubectl -n argocd rollout status deploy/my-release-argocd-server
+kubectl -n argocd rollout status sts/my-release-argocd-application-controller
+
+NODE_IP=$(kubectl get nodes -o jsonpath="{.items[0].status.addresses[0].address}")
+NODE_PORT=$(kubectl -n argocd get services my-release-argocd-server -o jsonpath="{.spec.ports[0].nodePort}")
+PASSWORD=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
+echo -e "\nServing ArgoCD at http://$NODE_IP:$NODE_PORT"
+echo "Credentials: user=admin, password=$PASSWORD"
+
+echo -e "\n[*] Install ArgoCD"
+kubectl create -f bootstrap.yaml
