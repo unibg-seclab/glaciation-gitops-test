6885a3ae84360350101dd3dc31eabc4b238237fb
Matthew Rossi <mattw.rossi@gmail.com>
2025-03-19 15:04:13 +0100

    Use Ingress to access ArgoCD Web UI

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,7 +10,6 @@ helm install \
     --namespace argocd \
     --set configs.repositories[0].type=git \
     --set configs.repositories[0].url=https://github.com/unibg-seclab/glaciation-gitops-test.git \
-    --set server.service.type=NodePort \
     --values values/argo-cd-values.yaml \
     my-release argo/argo-cd
 
@@ -23,10 +22,8 @@ kubectl -n argocd rollout status deploy/my-release-argocd-repo-server
 kubectl -n argocd rollout status deploy/my-release-argocd-server
 kubectl -n argocd rollout status sts/my-release-argocd-application-controller
 
-NODE_IP=$(kubectl get nodes -o jsonpath="{.items[0].status.addresses[0].address}")
-NODE_PORT=$(kubectl -n argocd get services my-release-argocd-server -o jsonpath="{.spec.ports[0].nodePort}")
 PASSWORD=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
-echo -e "\nServing ArgoCD at http://$NODE_IP:$NODE_PORT"
+echo -e "\nServing ArgoCD at http://argocd.integration"
 echo "Credentials: user=admin, password=$PASSWORD"
 
 echo -e "\n[*] Install ArgoCD application"
